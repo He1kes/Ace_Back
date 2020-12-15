fffee35fe77310b0abbd6d2033f51d656eb4c961
Heikes <1192676603@qq.com>
2020-12-15 14:59:26 +0800

    修改欢迎页，并完成房源，评论

---
+++ assets/js/heikes/commonDemo.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        option:"后缀",
+        allCheck:false,
+        pageActiveNum:1,
+        nagiList:[1,2,3,4,5],
+        menuActiveNum:1,
+        pageActiveTotal:5
+    },
+    methods:{
+        //复选框全选
+        allCheckBox:function () {
+            //选中为true,未选中为false
+            //console.log(event.target.checked);
+            this.allCheck = event.target.checked;
+        },
+        //当前选中的页数
+        pageActive:function(item){
+            this.pageActiveNum = item;
+        },
+        //当前选中菜单
+        menuActive:function (item) {
+            this.menuActiveNum = item;
+            console.log(this.menuActiveNum);
+        }
+    }
+})

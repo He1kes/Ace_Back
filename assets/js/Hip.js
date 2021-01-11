cd3316caec507aadb7b1e54d280ad92ec7c713e6
Heikes <1192676603@qq.com>
2021-01-11 17:57:07 +0800

    houseInfo add 2.0

--- assets/js/Hip.js
+++ assets/js/Hip.js
@@ -1,3 +1,3 @@
-var commentIp = "http://192.168.0.7:6606/consumer/comment";
-var houseIp = "http://192.168.0.7:6604/consumer/info";
-var typeIp = "http://192.168.0.7:6604/consumer/type"
+var commentIp = "http://192.168.0.101:6606/consumer/comment";
+var houseIp = "http://192.168.0.101:6604/consumer/info";
+var typeIp = "http://192.168.0.101:6604/consumer/type"

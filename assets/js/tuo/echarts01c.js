4f25a6e4ef7c0b9eac04e59986b7a2798ceabe1a
beiyan <758709763@qq.com>
2021-01-13 18:04:26 +0800

    商户、管理员的图表、仪表盘

--- assets/js/tuo/echarts01c.js
+++ assets/js/tuo/echarts01c.js
@@ -3,15 +3,16 @@ var initialDate = new Date();
 //地址
 var txq = txqIp;
 var orderPath = "/order/back";
+var orderIP="/order/front/private/";
 //token
 var token = localStorage.getItem("token");
+//用户id
+var userId;
 //图表1参数
 var dataYear1 = null;
 var dataMonth1 = null;
 var dataday1 = null;
-//图表2参数
-var dataYear2 = null;
-var dataMonth2 = null;
+
 //图表y轴数据
 var chartOneDataY;
 var chartTwoDataY;
@@ -69,27 +70,6 @@ function getDays(year,month) {
 }
 getDays(initialDate.getFullYear(),initialDate.getMonth()+1);
 
-//设置x轴月份数组(月入收益图表）
-var months = new Array();
-function getMonths(year) {
-    months = [];
-    var nowDate = new Date();
-    var nowYear = nowDate.getFullYear();
-    if(nowYear == year){
-        var nowMonth = nowDate.getMonth()+1;
-        dataMonth2 = nowMonth;
-        for(var i=1;i<=nowMonth;i++){
-            months[i-1] = i;
-        }
-    }else {
-        dataMonth2 = 12;
-        for(var i=1;i<=12;i++){
-            months[i-1] = i;
-        }
-    }
-}
-getMonths(initialDate.getFullYear());
-
 //年、月下拉框联动(年确定月）
 $("#businessesEchartsDiv01 .year").blur(function () {
     var year = $("#businessesEchartsDiv01").find(".year option:selected").val();
@@ -106,19 +86,43 @@ $("#selectYearMonth").click(function () {
     getChartOneData(dataYear1,dataMonth1,dataday1);
 })
 
-//获取年下拉框选定的值
-$("#selectYear").click(function () {
-    var year = $("#businessesEchartsDiv02").find(".year option:selected").val();
-    dataYear2 = year;
-    getMonths(year);
-    getChartTwoData(dataYear2,dataMonth2);
-})
+//设置x轴月份数组(月入收益图表）
+var months = new Array();
+months = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+//设置饼状图的数据
+var yuesdate = new Array();
+function setData() {
+    for(var i=1;i<=12;i++){
+        var datay = {};
+        datay.name = months[i-1]+"月";
+        datay.value = chartTwoDataY[i-1];
+        yuesdate.push(datay);
+    }
+    console.log(yuesdate);
+}
+
+
+//通过token拿userId
+function getUidByToken() {
+    $.ajax({
+        url:txq+orderIP+"/getUserIdByToken",
+        headers:{"token":localStorage.getItem("token")},
+        async:false,
+        success:function (data) {
+            userId = data.data;
+            console.log("getUserIdByToken:");
+            console.log(userId);
+        }
+    })
+}
+getUidByToken();
 
 //获取第一个图表的数据
-function getChartOneData(year,month,day) {
+function getChartOneData(year,month,day,lid) {
     $.ajax({
         url:txq+orderPath+"/getOrderMoneyDay",
-        data:{year:year,month:month,day:day},
+        data:{year:year,month:month,day:day,landId:lid},
         headers:{"token":localStorage.getItem("token")},
         async:false,
         success:function (data) {
@@ -131,14 +135,15 @@ function getChartOneData(year,month,day) {
 }
 
 //获取第二个图表的数据
-function getChartTwoData(year,month) {
+function getChartTwoData(year,month,lid) {
     $.ajax({
         url:txq+orderPath+"/getOrderMoneyMonth",
-        data:{year:year,month:month},
+        data:{year:year,month:month,landId:lid},
         headers:{"token":localStorage.getItem("token")},
         async:false,
         success:function (data) {
             chartTwoDataY = data;
+            setData();
             console.log("getChartTwoData:");
             console.log(data);
             drawEchartTwo();
@@ -153,10 +158,10 @@ function drawEchartOne() {
 // 指定图表的配置项和数据
     var option01 = {
         title: {
-            text: '平台每月日入流水'
+            text: '房租日收入额'
         },
         legend:{
-            data:["日入流水"],
+            data:["日收入额"],
             right:'10%'
         },
         tooltip: {
@@ -170,7 +175,7 @@ function drawEchartOne() {
             name:'（元）'
         },
         series: [{
-            name: '日入流水',
+            name: '日收入额',
             type: 'line',
             label:{
                 show:true
@@ -191,30 +196,30 @@ function drawEchartTwo() {
 // 指定图表的配置项和数据
     var option02 = {
         title: {
-            text: '平台每年月入流水'
+            text: '上年度租房月收入占比',
+            left: 'center'
         },
-        legend:{
-            data:["月入流水"],
-            right:'10%'
-        },
-        tooltip: {
-
-        },
-        xAxis: {
-            name:'（月份）',
-            data: months
-        },
-        yAxis: {
-            name:'（元）'
-        },
-        series: [{
-            name: '月入流水',
-            type: 'bar',
-            label:{
-                show:true
-            },
-            data: chartTwoDataY
-        }]
+        tooltip: {},
+        series: [
+            {
+                name: '当月收入',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '50%'],
+                data: yuesdate,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                },
+                label:{
+                    show:true,
+                    formatter: '{b} ({d}%)'
+                },
+            }
+        ]
     };
 
 // 使用刚指定的配置项和数据显示图表。
@@ -222,5 +227,5 @@ function drawEchartTwo() {
 }
 
 //初始图表
-getChartOneData(initialDate.getFullYear(),initialDate.getMonth()+1,initialDate.getDate());
-getChartTwoData(initialDate.getFullYear(),initialDate.getMonth()+1);
+getChartOneData(initialDate.getFullYear(),initialDate.getMonth()+1,initialDate.getDate(),userId);
+getChartTwoData(initialDate.getFullYear()-1,12,userId);
